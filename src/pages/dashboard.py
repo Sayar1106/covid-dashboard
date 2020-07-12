0f92ae0abe76a4e62ebaf495ff76a82033f76976
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-12 17:52:14 +0530

    Adding top 10 countries

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -23,7 +23,7 @@ def plot_snapshot_numbers(df, colors, date):
                       hovertemplate = 'Status: %{y} <br>Count: %{x:,.2f}'
                       )
     fig.update_layout(
-        title="Total count as on {}".format(date),
+        title="Total count as on {}".format(date.strftime("%d %B, %Y")),
         width=750,
         legend=dict(title="Status"),
         xaxis=dict(title="Count", showgrid=False),
@@ -31,8 +31,43 @@ def plot_snapshot_numbers(df, colors, date):
     )
     st.plotly_chart(fig)
 
+def plot_top_countries(df, colors, date):
+    st.spinner("Rendering chart...")
+    temp = df.groupby("Country_Region").agg({"Confirmed": "sum",
+                                             "Deaths": "sum",
+                                             "Recovered": "sum",
+                                             "Active": "sum"})
+    colors = px.colors.qualitative.Pastel1
+    fig = make_subplots(2,2, subplot_titles=["Top 10 counties by cases",
+                                             "Top 10 counties by deaths",
+                                             "Top 10 counties by recoveries",
+                                             "Top 10 countries by active cases"])
+    fig.append_trace(go.Bar(x=temp["Confirmed"].nlargest(n=10),
+                            y=temp["Confirmed"].nlargest(n=10).index,
+                            orientation='h'),
+                     row=1, col=1)
+
+    fig.append_trace(go.Bar(x=temp["Deaths"].nlargest(n=10),
+                            y=temp["Deaths"].nlargest(n=10).index,
+                            orientation='h'),
+                    row=2, col=1)
+
+    fig.append_trace(go.Bar(x=temp["Recovered"].nlargest(n=10),
+                            y=temp["Recovered"].nlargest(n=10).index,
+                            orientation='h'),
+                    row=1, col=2)
+
+    fig.append_trace(go.Bar(x=temp["Active"].nlargest(n=10),
+                            y=temp["Active"].nlargest(n=10).index,
+                        orientation='h'),
+                        row=2, col=2)
+
+    fig.update_layout(height=800,
+                      width=1100,)
+    st.plotly_chart(fig)
 
 def main():
+    pio.templates.default = "plotly_dark"
     date = datetime.today()
     DATA_URL = ""
     try:
@@ -44,4 +79,5 @@ def main():
     df = load_data(DATA_URL)
     st.sidebar.markdown("## Country")
     plot_snapshot_numbers(df, px.colors.qualitative.D3, date.date())
+    plot_top_countries(df, px.colors.qualitative.D3, date.date())
     st.sidebar.selectbox("", df["Country_Region"].unique())
