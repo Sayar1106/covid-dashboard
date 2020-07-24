5d854a9f528632f5bc2431081fe0153ac4f3f967
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-24 13:48:58 +0530

    Moving about and contribute to page

--- src/pages/about.py
+++ src/pages/about.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 
 def main():
-    st.sidebar.info(
+    st.title("About")
+    st.info(
         """
         This app is maintained by Sayar Banerjee. You can learn more about me at
         [sayar1106.github.io](https://sayar1106.github.io).
--- src/pages/contribute.py
+++ src/pages/contribute.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 
 def main():
-    st.sidebar.info("Feel free to contribute to this open source project. The github link can be found "
+    st.title("Contribute")
+    st.info("Feel free to contribute to this open source project. The github link can be found "
                     "[here](https://github.com/Sayar1106/covid-dashboard)")
