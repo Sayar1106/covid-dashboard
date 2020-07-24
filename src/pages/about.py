e708783c1d22e6f20e8a50ab731b3c61b54fc10d
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-24 13:43:19 +0530

    Adding About and Contribute

---
+++ src/pages/about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.sidebar.info(
+        """
+        This app is maintained by Sayar Banerjee. You can learn more about me at
+        [sayar1106.github.io](https://sayar1106.github.io).
+        """
+)
---
+++ src/pages/contribute.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def main():
+    st.sidebar.info("Feel free to contribute to this open source project. The github link can be found "
+                    "[here](https://github.com/Sayar1106/covid-dashboard)")
