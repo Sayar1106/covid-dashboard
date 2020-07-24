5edf2d44a2c0a306a3d0fa3cc1832552523f530a
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-24 13:09:44 +0530

    removing app file

--- app_test.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/07-18-2020.csv")
-st.write(df)
